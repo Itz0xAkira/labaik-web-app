c98c08c6bb83c26c0b6416e629dc92d723132ed3
0xAkira <eweeda172@gmail.com>
2024-01-17 15:53:50 +0100

    Update config.ts

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 //Production
-const firebaseConfig = {
-  apiKey: "AIzaSyAPljtE4WMskFrwLZSjRRyZbI1UQKFR5wY",
-  authDomain: "makkah-hotels-77d1a.firebaseapp.com",
-  projectId: "makkah-hotels-77d1a",
-  storageBucket: "makkah-hotels-77d1a.appspot.com",
-  messagingSenderId: "541839871571",
-  appId: "1:541839871571:web:d0c4e0b069b6c9b93bc0a6",
-  measurementId: "G-RCGCWJXDW8",
-};
+const firebaseConfig = ""
 
 // Initialize Firebase
 
