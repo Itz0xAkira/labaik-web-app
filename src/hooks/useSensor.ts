9d8ce93f9a7d5ffdd15474eb70efd9a5915c7380
0xAkira <eweeda172@gmail.com>
2024-01-17 15:54:15 +0100

    Update useSensor.ts

--- src/hooks/useSensor.ts
+++ src/hooks/useSensor.ts
@@ -1,45 +1,45 @@
-// import { useEffect, useState } from "react";
-// import { Gyroscope } from "expo-sensors";
-// import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import { Gyroscope } from "expo-sensors";
+import * as Location from "expo-location";
 
-// export const useSensor = ({ mode }: { mode: "slow" | "fast" }) => {
-//   const [{ x, y, z }, setData] = useState({
-//     x: 0,
-//     y: 0,
-//     z: 0,
-//   });
-//   const [subscription, setSubscription] = useState(null);
+export const useSensor = ({ mode }: { mode: "slow" | "fast" }) => {
+  const [{ x, y, z }, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
 
-//   const _slow = () => Gyroscope.setUpdateInterval(1000);
-//   const _fast = () => Gyroscope.setUpdateInterval(60);
+  const _slow = () => Gyroscope.setUpdateInterval(1000);
+  const _fast = () => Gyroscope.setUpdateInterval(60);
 
-//   const _subscribe = () => {
-//     if (mode == "fast") {
-//       _fast();
-//     } else {
-//       _slow();
-//     }
-//     setSubscription(
-//       Gyroscope.addListener((gyroscopeData) => {
-//         setData(gyroscopeData);
-//       })
-//     );
-//   };
+  const _subscribe = () => {
+    if (mode == "fast") {
+      _fast();
+    } else {
+      _slow();
+    }
+    setSubscription(
+      Gyroscope.addListener((gyroscopeData) => {
+        setData(gyroscopeData);
+      })
+    );
+  };
 
-//   const _unsubscribe = () => {
-//     subscription && subscription.remove();
-//     setSubscription(null);
-//   };
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
 
-//   useEffect(() => {
-//     _subscribe();
+  useEffect(() => {
+    _subscribe();
 
-//     return () => _unsubscribe();
-//   }, []);
+    return () => _unsubscribe();
+  }, []);
 
-//   return {
-//     x,
-//     y,
-//     z,
-//   };
-// };
+  return {
+    x,
+    y,
+    z,
+  };
+};
